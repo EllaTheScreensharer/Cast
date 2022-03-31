3598a20c26d94c4e3188e1cfc9fe59bdecac81cf
Ella <95990321+EllaTheScreensharer@users.noreply.github.com>
2022-03-31 22:10:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 import webbrowser
+import sys, subprocess, random, json, asyncio, os, time
+try:
+    import colorama, pyfade, discord
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'colorama'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'pyfade'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord.py'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord'])
+from colorama import Fore
+from datetime import datetime
+from discord.ext import commands
+import os
+def addToClipBoard(text):
+    command = 'echo ' + text.strip() + '| clip'
+    os.system(command)
 
-webbrowser.open('https://anonfiles.com/v6v0i9Rdxc/Cast_Scanner_exe')
+# Example
+
+while True:
+    for i in range(1, 100):
+        print('\n')
+    print(pyfade.Fade.Horizontal(pyfade.Colors.blue_to_cyan, '''     ▄████▄   ▄▄▄        ██████ ▄▄▄█████▓
+    ▒██▀ ▀█  ▒████▄    ▒██    ▒ ▓  ██▒ ▓▒
+    ▒▓█    ▄ ▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░
+    ▒▓▓▄ ▄██▒░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ 
+    ▒ ▓███▀ ░ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ 
+    ░ ░▒ ▒  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   
+    ░  ▒     ▒   ▒▒ ░░ ░▒  ░ ░    ░    
+    ░          ░   ▒   ░  ░  ░    ░      
+    ░ ░            ░  ░      ░           
+    ░                                    '''))
+    for i in range(1, 8):
+        print('\n')
+    print(pyfade.Fade.Horizontal(pyfade.Colors.purple_to_red, """This is the OFFICIAL installer for the Cast Scanner. Choose from the options below
+
+
+    [1] Copy github to clipboard    [2] Proceed to install Cast Scanner    [3] Not Complete\n\n"""))
+    choice1 = input('\nInput: ')
+    if choice1 == '1':
+        addToClipBoard('https://github.com/EllaTheScreensharer/Cast')
+
+    if choice1 == '2':
+        webbrowser.open('https://cdn-103.anonfiles.com/v6v0i9Rdxc/620ee6a9-1648758173/Cast%20Scanner.exe')
